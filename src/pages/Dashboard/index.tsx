0b8cf0f936637a1edbb50349d2f95d3cf85a6416
Leandro Souza <leandror.dev@gmail.com>
2020-04-29 10:41:53 -0300

    api connect

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,7 +35,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const response = await api.get('/products');
+
+      setProducts(response.data);
     }
 
     loadProducts();
